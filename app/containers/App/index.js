01af9f84e5913e195740cd39a054af3bf2d387ed
kmorgan <kiki.morgan13@gmail.com>
2017-08-21 18:20:09 +1000

    basic react app

---
+++ app/containers/App/index.js
@@ -0,0 +1,26 @@
+
+/*
+
+The skeleton around the actual page, should only contain code
+that will be seen on all pages (if we have multiple pages)
+
+*/
+
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+//import components
+//imported using resolve.alias to shorten import name
+//import LocalTime from 'Components/clock/clock';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+       <h1> please work </h1>
+      </div>
+    )
+  }
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const webpack = require('webpack');
+
+//plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+//FriendlyErrorsWebpackPlugin
+//ExtractTextPlugin
+//NoEmitOnErrorsPlugin
+//NamedModulesPlugin
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './app/app.js'
+  ],
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app_bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader' ]}
+    ], //end of rules
+  },//end of module
+
+  resolve: {
+    alias: {
+      Components: path.resolve(__dirname, 'app/components/'),
+      Containers: path.resolve(__dirname, 'app/containers/')
+    },
+    extensions: ['*', '.js', '.jsx']
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+
+  devtool: 'cheap-module-eval-source-map',
+
+  devServer: {
+    hot: true,
+    open: true,
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  },
+
+  watch: true,
+
+  watchOptions: {
+    ignored: /node_modules/
+  }
+
+}//end of module exports

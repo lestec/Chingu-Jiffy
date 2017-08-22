b0df2d96ca143b21fc1a039dd939fdc251bb906c
kmorgan <kiki.morgan13@gmail.com>
2017-08-22 12:31:43 +1000

    remove my components

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -11,16 +11,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 //import components
-//imported using resolve.alias to shorten import name
-import LocalTime from 'Components/Clock/index';
-import TodoMain from 'Components/TodoList/index';
+//import using resolve.alias to shorten import name
+
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <LocalTime />
-        <TodoMain />
+        <h1> Welcome to Jiffy </h1>
+        <p> run yarn run start to open webpack-dev-server, <br/>
+        include your code in a new folder in the components folder, <br/>
+        don't forget to import your component in the containers/App/index.js
+        file, and then call it in the render section. <br/>
+        and checkout webpack.config.js for resolve aliases that can be used to
+        shorted import file name <br/>
+        and this dev-server does have hotloading and other cool packages, check out package.json
+        </p>
       </div>
     )
   }
